7a09ff887f7e7d71764fc3f6da9031385378404c
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-09-09 10:27:54 -0500

    Update even_odd.js

--- even_odd.js
+++ even_odd.js
@@ -3,8 +3,10 @@ function check_even(n){
  if(n === 0)
   return "even :" + true;
  else if(n === 1)
-  return "even :" + false;
+  return "even :" + false + "therefore odd";
+ else if(n < 0)
+  return " ?? ";
  else
   return check_even(n - 2);
 }
-  check_even(15);
+  check_even(75);
